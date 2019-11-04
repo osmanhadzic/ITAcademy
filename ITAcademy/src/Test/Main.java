409f2361c997829638db221302dc9dce89829219
osmanhadzic <Allahjejedan995>
2019-11-04 08:07:30 +0100

    update

--- ITAcademy/src/Test/Main.java
+++ ITAcademy/src/Test/Main.java
@@ -1,10 +1,26 @@
 package Test;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello word, my name iz Osman.");
+		Scanner in = new Scanner(System.in);
+		int c;
+		System.out.println("Unsite brojnik: ");
+		int a = in.nextInt();
+		System.out.println("Unsite nazivnik: ");
+		int b = in.nextInt();
+		
+		try {
+			c = a / b;
+			System.out.println("Rezultat je: " + c);
+		}catch(Exception e ){
+			System.out.println("Nazivnik je nula!!! ");
+		}
+		
+		in.close();
 
 	}
 
